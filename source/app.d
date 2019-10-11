d50a77277f64652d9291fcb7804c48ea9367f88e
Pablo De Nápoli <pdenapo@gmail.com>
2019-10-11 18:29:11 -0300

    Try using host 0.0.0.0 like in
    https://help.heroku.com/P1AVPANS/why-is-my-node-js-app-crashing-with-an-r10-error

--- source/app.d
+++ source/app.d
@@ -1,19 +1,22 @@
 import vibe.vibe;
 import std.process;
+import std.conv;
+
+HTTPServerSettings settings;
 
 void main()
 {
-	auto settings = new HTTPServerSettings;
+	settings = new HTTPServerSettings ;
 	 // Provide a default port in case of the $PORT variable isn't set.
         settings.port = environment.get("PORT", "8080").to!ushort;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
+	settings.bindAddresses = [ "127.0.0.1","0.0.0.0"];
 	listenHTTP(settings, &hello);
 
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+	logInfo("Heroku test application started! Listening to port " ~ to!string(settings.port));
 	runApplication();
 }
 
 void hello(HTTPServerRequest req, HTTPServerResponse res)
 {
-	res.writeBody("Hello, World!");
+	res.writeBody("Heroku test application listening to port "~ to!string(settings.port));
 }
