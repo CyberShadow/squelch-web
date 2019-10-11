fa3301b53308df5ced5a5d27a64855fff709bc43
Pablo De Nápoli <pdenapo@gmail.com>
2019-10-11 18:15:27 -0300

    Use PORT enviroment variable.

--- source/app.d
+++ source/app.d
@@ -1,9 +1,11 @@
 import vibe.vibe;
+import std.process;
 
 void main()
 {
 	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
+	 // Provide a default port in case of the $PORT variable isn't set.
+        settings.port = environment.get("PORT", "8080").to!ushort;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
 	listenHTTP(settings, &hello);
 
